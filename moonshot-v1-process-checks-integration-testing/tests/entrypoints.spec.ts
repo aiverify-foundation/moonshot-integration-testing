f4f5af873f9b23c987584ec5c39163b085bc202e
imda-jacksonboey <jackson_boey@imda.gov.sg>
2025-06-03 20:02:43 +0800

    Update test case

--- moonshot-v1-process-checks-integration-testing/tests/entrypoints.spec.ts
+++ moonshot-v1-process-checks-integration-testing/tests/entrypoints.spec.ts
@@ -3694,7 +3694,7 @@ test('test_complete_process_checks_page_resume_session', async ({page}) => {
     // Back to Home Page Attempt to Resume Session
     await expect(page.getByRole('heading', {name: 'Welcome to Process Checks for'})).toBeVisible({timeout: 90000});
     //Attempt to Click Continue where you left off btn
-    await page.getByTestId('stMainBlockContainer').getByTestId('stBaseButton-secondary').click();
+    await page.locator('text=Continue Where You Left Off').click();
     //Assert Resume button is disabled
     await expect(page.getByTestId('stDialog').getByTestId('stBaseButton-primary')).toBeDisabled();
     //Select Dropdownlist for Session
@@ -3707,7 +3707,7 @@ test('test_complete_process_checks_page_resume_session', async ({page}) => {
 
     //Attempt to Resume Session
     //Attempt to Click Continue where you left off btn
-    await page.getByTestId('stMainBlockContainer').getByTestId('stBaseButton-secondary').click();
+    await page.locator('text=Continue Where You Left Off').click();
     //Assert Resume button is disabled
     await expect(page.getByTestId('stDialog').getByTestId('stBaseButton-primary')).toBeDisabled();
     //Select Dropdownlist for Session
